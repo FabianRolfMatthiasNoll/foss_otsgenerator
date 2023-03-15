1494d6cb82eee38446d688f81f7366e7d319ed9a
fanoll <Fabian.Noll@googlemail.com>
2023-03-15 10:32:41 +0100

    added: factory for language handler added: interface for language handlers

--- internal/manager.go
+++ internal/manager.go
@@ -2,10 +2,15 @@ package internal
 
 import (
 	"fmt"
+	"foss_otsgenerator/internal/handlers"
 	"foss_otsgenerator/internal/models"
 
 	"github.com/TwiN/go-color"
 )
+
+type Handler_Interface interface {
+	GetPackageInfo([]models.Dependency) []models.Module
+}
 
 type Manager struct {
 }
@@ -15,10 +20,24 @@ func (Manager) GenerateOTS(inputPath, configPath, outputPath string) {
 	sbom, err := sbomManager.ReadAndMapSBOM(inputPath)
 	if err != nil {
 		fmt.Println("[", color.Colorize(color.Red, "Err"), "] ", err)
-		//return
+		return
 	}
 
-	for lan, dep := range sbom.Dependencies {
-		fmt.Println(lan, "||", dep)
+	var handler Handler_Interface
+	for lang, deps := range sbom.Dependencies {
+		switch {
+		case lang == "nuget":
+			handler = handlers.Nuget{}
+		case lang == "golang":
+			handler = handlers.Go{}
+		case lang == "npm":
+			handler = handlers.NPM{}
+		case lang == "conan":
+			handler = handlers.Conan{}
+		default:
+			fmt.Printf("Unsupported language")
+		}
+
+		handler.GetPackageInfo(deps)
 	}
 }
