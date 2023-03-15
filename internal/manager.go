403327cf6d61a879623d6ddcd41d3c802c93271d
fanoll <Fabian.Noll@googlemail.com>
2023-03-15 09:01:59 +0100

    added: basic functionality reads the sbom and then goes through it values. From here on out the deps can be given to their handlers

--- internal/manager.go
+++ internal/manager.go
@@ -1,1 +1,24 @@
 package internal
+
+import (
+	"fmt"
+	"foss_otsgenerator/internal/models"
+
+	"github.com/TwiN/go-color"
+)
+
+type Manager struct {
+}
+
+func (Manager) GenerateOTS(inputPath, configPath, outputPath string) {
+	var sbomManager models.SBOM
+	sbom, err := sbomManager.ReadAndMapSBOM(inputPath)
+	if err != nil {
+		fmt.Println("[", color.Colorize(color.Red, "Err"), "] ", err)
+		//return
+	}
+
+	for lan, dep := range sbom.Dependencies {
+		fmt.Println(lan, "||", dep)
+	}
+}
