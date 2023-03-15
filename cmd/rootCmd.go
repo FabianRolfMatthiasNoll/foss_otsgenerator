13bc26eb31a76779d565f62a4f3d1a1bfc2ea60e
fanoll <Fabian.Noll@googlemail.com>
2023-03-15 09:00:44 +0100

    added: cmd to initalize the ots generation

--- cmd/rootCmd.go
+++ cmd/rootCmd.go
@@ -1,17 +1,32 @@
 package cmd
 
 import (
+	"foss_otsgenerator/internal"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "FOSS_toolconverter",
+	Use:   "foss_toolconverter",
 	Short: "Converts the Output of a tool to usable dependency format",
 }
 
+var initCmd = &cobra.Command{
+	Use:   "init",
+	Short: "fosser_otsgenerator init [Path/To/SBOM] [Path/To/Config] [Path/For/Output]",
+	Args:  cobra.ExactArgs(3),
+	Run: func(cmd *cobra.Command, args []string) {
+		inputPath := args[0]
+		configPath := args[1]
+		outputPath := args[2]
+		var manager internal.Manager
+		manager.GenerateOTS(inputPath, configPath, outputPath)
+	},
+}
+
 func init() {
+	rootCmd.AddCommand(initCmd)
 }
 
 func Execute() {
